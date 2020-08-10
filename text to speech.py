27a91c5f1211ab0e7860063cf0d72c6aa61ecbb6
pavithranatarajan2000 <58357646+pavithranatarajan2000@users.noreply.github.com>
2020-08-10 16:04:50 +0530

    Add files via upload

---
+++ text to speech.py
@@ -0,0 +1,8 @@
+import pyttsx3
+test = "Hii Friends!!!! Happy friendship day to all"
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+en_voice_id = "com.apple.speech.synthesis.voice.Alex"
+engine.setProperty('voice', en_voice_id)
+engine.say(test)
+engine.runAndWait()
